580bf0c57c034a33fb481f21c9f2ce8ea5ab0f2d
jacknero2 <jnero@nd.edu>
2025-02-07 17:26:32 -0500

    RUNNING TESTS: update #''

--- build/update_tests.sh
+++ build/update_tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 updateNUM=0
+PID=1861859
 
-sleep 5
+while kill -0 "$PID" 2>/dev/null; do
+    sleep 5
 
-git add .
-git commit -m "RUNNING TESTS: update #'$updateNum'"
-git push -u origin experiments
+    git add .
+    git commit -m "RUNNING TESTS: update #'$updateNum'"
+    git push -u origin experiments
+done
