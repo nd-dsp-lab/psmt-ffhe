88773c237713863598e80423dbedaca7c82650a9
jacknero2 <jnero@nd.edu>
2025-02-07 17:30:21 -0500

    RUNNING TESTS: update #0

--- build/update_tests.sh
+++ build/update_tests.sh
@@ -3,10 +3,15 @@
 updateNUM=0
 PID=1861859
 
+# Loop until the process with PID stops
 while kill -0 "$PID" 2>/dev/null; do
-    sleep 5
 
     git add .
-    git commit -m "RUNNING TESTS: update #'$updateNum'"
+    git commit -m "RUNNING TESTS: update #$updateNUM"
     git push -u origin experiments
+
+    updateNUM=$((updateNUM + 1))
+    sleep 60  # Check every 60 seconds
 done
+
+echo "Process $PID has stopped. Script finished."
