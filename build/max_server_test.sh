211d37407ed0f9402552319bdc99a74ce7723ec0
jacknero2 <jnero@nd.edu>
2025-03-11 10:21:48 -0400

    Update to tests

--- build/max_server_test.sh
+++ build/max_server_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir test-results
-recordFile="test-results/test.csv"
+recordFile="test-results/test2.csv"
 
 echo "senders, time_elapsed" >> $recordFile
 
-for i in {1..4096}; do
-    if ((i % 100 == 0)); then
+for i in {1..10000}; do
+    if ((i % 10 == 0)); then
         totalTime=$(./main -numItem 15 -lenData 2 -numPack 1 -numParties $i -numAgg 10 -alpha 5 -interType CPI  -allowIntersection 1 | grep "Total Time Elapsed" | awk '{print $5}')
         echo "${i},${totalTime}" >> $recordFile
     fi
